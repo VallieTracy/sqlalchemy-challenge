3dbb4f2f782d9ad6a9e284828707c03126e7a511
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-02-19 11:01:05 -0600

    saving work with geronimo

--- app.py
+++ app.py
@@ -1,9 +1,53 @@
 from flask import Flask, jsonify
+import date as dt 
+import numpy as np  
+import pandas as pd 
+import sqlalchemy  
+from sqlalchemy.ext.automap import automap_base 
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
 
 app = Flask(__name__)
 
 # Define what to do when a user hits the index route
 @app.route("/")
+def home():
+    return(
+        "Welcome to Hawaii Climate Analysis API!<br/>"
+        "available routes:<br/>"
+        "<a href='/api/v1.0/precipitation'>Precipitation<a/>"
+        "<a href='/api/v1.0/stations'>Stations<a/>"
+        "<a href='/api/v1.0/tobs'>Tobs<a/>"
+        "<a href='/api/v1.0/<start>/<end>'>Start_End<a/>"
+    )
+
+    # Define what to do when a user hits the index route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return()
+    
+# Define what to do when a user hits the index route
+@app.route("/api/v1.0/stations")
+def stations():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+# Define what to do when a user hits the index route
+@app.route("/api/v1.0/tobs")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+# Define what to do when a user hits the index route
+@app.route("/api/v1.0/<start>/<end>")
 def home():
     print("Server received request for 'Home' page...")
     return "Welcome to my 'Home' page!"
